78919a7f88e71a9c682c2f779a6ae7a53f1bfe13
Jacqueline S <101556121+jacky-schfr@users.noreply.github.com>
2022-05-22 10:21:58 +0200

    added character movement and treasure chest.

--- Wonder Lost/Assets/Scripts/PlayerMovementController.cs
+++ Wonder Lost/Assets/Scripts/PlayerMovementController.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovementController : MonoBehaviour
 {
+    public float speed = 15.0f;
+    private Rigidbody2D rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody2D> ();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 dir = new Vector2(h, v);
+        rigidbody.velocity = dir.normalized * speed;
     }
 }
