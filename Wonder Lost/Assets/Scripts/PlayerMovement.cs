d0c2ebfc85d9afc45857f513c844a138ff898248
Jacqueline S <101556121+jacky-schfr@users.noreply.github.com>
2022-06-06 11:19:41 +0200

    added back player movement. added camera movement that follows player.

--- Wonder Lost/Assets/Scripts/PlayerMovement.cs
+++ Wonder Lost/Assets/Scripts/PlayerMovement.cs
@@ -2,25 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectMovement1 : MonoBehaviour
+
+public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 15.0f;
+    [SerializeField] private float speed = 15.0f;
     private Rigidbody2D rigidbody;
 
-    // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
         Vector2 dir = new Vector2(h, v);
-        rigidbody.velocity = dir.normalized * -speed;
+        rigidbody.velocity = dir.normalized * speed;
     }
+
 }
